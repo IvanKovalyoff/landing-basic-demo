060008de0df251f745e3ab4844b4f5682281cdd9
Ivan <kovalevivan420@gmail.com>
2026-03-05 11:15:18 +0100

    changing redirection path

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -65,7 +65,7 @@ export default function Contact() {
               method="POST"
               data-netlify="true"
               data-netlify-honeypot="bot-field"
-              action="/thanks"
+              action="/thanks.html"
               onSubmit={onSubmit}
             >
               <input type="hidden" name="form-name" value={site.contact.form.name} />
